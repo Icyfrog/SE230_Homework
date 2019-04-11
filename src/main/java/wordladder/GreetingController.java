12e845e68d97d9e2e972e8457a49732495f8df4c
IcyFrog <sjtu-yqc@sjtu.edu.cn>
2019-04-11 10:35:27 +0800

    more types to send param

--- src/main/java/wordladder/GreetingController.java
+++ src/main/java/wordladder/GreetingController.java
@@ -1,6 +1,6 @@
 package wordladder;
-//import java.util.concurrent.atomic.AtomicLong;
-//import org.springframework.web.bind.annotation.PathVariable;
+import java.util.concurrent.atomic.AtomicLong;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,9 +12,14 @@ import org.springframework.web.bind.annotation.RestController;
         //private final AtomicLong counter = new AtomicLong();
 
         @RequestMapping("/Ladder")
-        public String ladder1(@RequestParam(value = "b") String beginw,
-                              @RequestParam(value = "e") String endw) {
+        public String ladder1(@RequestParam(name = "bc") String beginw,
+                              @RequestParam(name = "ec") String endw) {
             Greeting A = new Greeting();
             return A.ss(beginw,endw);
+        }
+        @RequestMapping("/Ladder/{beginword}/{endword}")
+        public String ladder2(@PathVariable String beginword, @PathVariable String endword){
+            Greeting A = new Greeting();
+            return A.ss(beginword,endword);
         }
     }
