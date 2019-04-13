fa74aa6787be5f3b3401eaa38bfa46ba80ab9f70
IcyFrog <sjtu-yqc@sjtu.edu.cn>
2019-04-13 12:25:26 +0800

    test unfinished

--- src/main/java/hello/HelloWorldApplication.java
+++ src/main/java/hello/HelloWorldApplication.java
@@ -1,7 +1,11 @@
 package hello;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.actuate.autoconfigure.endpoint.condition.ConditionalOnEnabledEndpoint;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
 public class HelloWorldApplication {
@@ -10,4 +14,13 @@ public class HelloWorldApplication {
         SpringApplication.run(HelloWorldApplication.class, args);
     }
 
+    @Configuration
+    static class AuthorEndPointConfiguration{
+        @Bean
+        @ConditionalOnMissingBean
+        @ConditionalOnEnabledEndpoint
+        public AuthorEndPoint getAuthor(){
+            return new AuthorEndPoint();
+        }
+    }
 }
