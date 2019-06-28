afcd327cc1db844776b5e2100fcb9750c1561fbd
Hover your Wings <43701782+Icyfrog@users.noreply.github.com>
2019-06-28 13:55:54 +0800

    Add files via upload

---
+++ 10Tasks/task4/src/main/java/task4/container/Controller.java
@@ -0,0 +1,38 @@
+package task4.container;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+    @RequestMapping("/firsttest")
+    public void easytest() throws InterruptedException{
+        Consumer consumer = new Consumer("1");
+        Producer producer = new Producer("2");
+
+        /* nothing to get */
+        consumer.get();
+
+        producer.put("A");
+        /* get A */
+        consumer.get();
+        /*  get nothing */
+        consumer.get();
+
+        producer.put("B");
+        Thread.sleep(31000);
+        /* get nothing because of timeout */
+        consumer.get();
+    }
+
+    @RequestMapping("secondtest")
+    public void test(){
+        /* multiple consumers and producers */
+        for(Integer i = 0; i<5; i++){
+            Consumer consumer = new Consumer("c"+i);
+            Producer producer = new Producer("p"+i);
+            producer.start();
+            consumer.start();
+        }
+    }
+}
